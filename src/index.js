478fa626b12fd9fc1027a5e57b8ac2107f778be1
Shanukumar2929 <shanukumar2929@gmail.com>
2024-06-28 10:14:37 +0530

    added second lecture

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const {PORT} = require('./config/serverConfig');
+
+const setupAndStartServer =  async () => {
+
+    // create a express object
+    const app = express();
+   
+
+
+    app.listen(PORT, () => {
+        console.log(`Server started at ${PORT}`);
+        console.log(process);
+    });
+
+
+
+}
